d0c0bb38baed1465ed1a6a2b6287d12e3c5ae120
Samibo-cmd <samueleibo12@gmail.com>
2022-06-23 10:54:55 +0100

    first upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+let name = prompt("Enter your name");
+let height = prompt("Enter your hright");
+let country = prompt("Enter your country");
+
+document.write("Name: ", name + "<br>" + "Height: ", height + "<br>" + "Country: ", country); 
+
+
